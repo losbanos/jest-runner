d20688366e794fe1b7036634d600eb56568768e9
kimtaehoon <kimtaehoon@smilegate.com>
2019-07-01 10:20:48 +0900

    [ADD] complete multiple page set

--- src/components/common/Helpers.js
+++ src/components/common/Helpers.js
@@ -3,3 +3,42 @@ export const getLength = function (list) {
     return list === null ? undefined : list.length;
 };
 
+const { log } = console;
+log('------- Arr -------');
+const arr = [1, 2, 3];
+const itr = arr[Symbol.iterator]();
+for (const i of itr) {
+    log(i);
+}
+log('------- Set -------');
+const set = new Set([1, 2, 3]);
+const setItr = set[Symbol.iterator]();
+setItr.next();
+for (const a of setItr) {
+    log(a);
+}
+
+log('------- Map -------');
+const map = new Map([['a', 1], ['b', 2], ['c', 3]]);
+for (const b of map) {
+    log(b);
+}
+log('-------- Custom --------');
+const iterable = {
+    [Symbol.iterator]() {
+        let i = 3;
+        return {
+            next() {
+                return i === 0 ? { done: true } : {
+                    value: i--, done: false
+                };
+            }
+        };
+    }
+};
+
+const iterator = iterable[Symbol.iterator]();
+log(iterator.next());
+log(iterator.next());
+
+log(parseFloat('14.5px'));
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <p>标点符号。</p>
+    </div>
 </template>
 <script>
 import { getFilmName, getPokemonInfo } from '@/components/sw/SWRequest';
@@ -18,23 +19,6 @@ export default {
         this.iteratable();
     },
     methods: {
-        iteratable() {
-            const arr = [1, 2, 3];
-            arr[Symbol.iterator] = null;
-            for (let a of arr) {
-                log(a);
-            }
-
-            const set = new Set([1, 2, 3]);
-            set[Symbol.iterator] = null;
-            for (const i of set) {
-                log(i);
-            }
-            const map = new Map([['a', 1], ['b', 2]]);
-            for (const a of map) {
-                log(a);
-            }
-        },
         requestTest() {
             const data = getFilmName('ab');
             // data.then(res => res.json()).then(data => console.log(data));
