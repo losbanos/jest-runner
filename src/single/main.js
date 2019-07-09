d5a3b0b2c52a393fc9f9ee1e8ccdcb059c36b915
kimtaehoon <kimtaehoon@smilegate.com>
2019-07-09 11:27:54 +0900

    [EDU] dom 연동 완료

--- src/single/main.js
+++ src/single/main.js
@@ -1,5 +1,8 @@
 import ClickCountModel from './ClickCountModel';
 import ClickCountView from './view/ClickCountView';
 
-// ClickCountModel.ClickCounter();
-// ClickCountView.ClickCountView();
+const counter = ClickCountModel.ClickCounter();
+const triggerEl = document.querySelector('#btn_increase');
+const updateEl = document.querySelector('#counter_display');
+const view = ClickCountView.initView(counter, { updateEl, triggerEl });
+view.updateView();
