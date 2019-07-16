5ec2a51609455aeb80ded645fb196fced34f4a39
KimTaeHoon <wwwholakr@gmail.com>
2019-07-17 00:03:50 +0900

    [EDU] Generator

--- src/components/fp/fp.js
+++ src/components/fp/fp.js
@@ -1,12 +1,29 @@
 const { log } = console;
-const els = document.querySelectorAll('*');
-const iterEls = els[Symbol.iterator]();
-log('next = ', iterEls.next());
-log('next = ', iterEls.next());
-log('next = ', iterEls.next());
-log('next = ', iterEls.next());
-log('next = ', iterEls.next());
-log('next = ', iterEls.next());
-log('next = ', iterEls.next());
-log('next = ', iterEls.next());
+// const els = document.querySelectorAll('*');
+// const iterEls = els[Symbol.iterator]();
+// log('next = ', iterEls.next());
+// log('next = ', iterEls.next());
+// log('next = ', iterEls.next());
+// log('next = ', iterEls.next());
 // for (const e of els) { log(e); }
+
+function* gener() {
+    yield 1;
+    if (false) {
+        yield 2;
+    }
+    yield 3;
+    return 100;
+}
+
+const iterGen = gener();
+log(iterGen[Symbol.iterator]() === iterGen);
+log(iterGen.next());
+log(iterGen.next());
+log(iterGen.next());
+log(iterGen.next());
+log(iterGen.next());
+
+for (const c of gener()) {
+    log(c);
+}
