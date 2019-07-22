48700f9d3e8f659d84a1d9ceec281b68be2c1f2e
KimTaeHoon <wwwholakr@gmail.com>
2019-07-23 00:04:33 +0900

    [EDU] reduce complete

--- src/components/fp/fp.js
+++ src/components/fp/fp.js
@@ -56,3 +56,82 @@ log(tail);
 // for (const n of odds(20)) {
 //     log(n);
 // }
+log('------------------define map function -------------');
+const products = [
+    { name: '반팔티', price: 15000 },
+    { name: '긴팔티', price: 24000 },
+    { name: '셔츠', price: 30000 },
+    { name: '반바지', price: 17000 },
+    { name: '체크바지', price: 25000 }
+];
+const map = (iterator, func) => {
+    const result = [];
+    for (const item of iterator) {
+        result.push(func(item));
+    }
+    return result;
+};
+if (!global) {
+    log(map(document.querySelectorAll('*'), el => el.nodeName));
+};
+if (!global) {
+    const els = document.querySelectorAll('*')[Symbol.iterator]();
+    log(els.next());
+    log(els.next());
+    log(els.next());
+}
+function* htmlElement() {
+    yield 2;
+    if (false) yield 4;
+    yield 5;
+};
+
+log(map(htmlElement(), n => n * 2));
+
+log('------------------ Native Map Constructor --------------');
+const m = new Map();
+m.set('a', 10);
+m.set('b', 20);
+const mIter = m[Symbol.iterator]();
+log(mIter.next());
+// log(mIter.next());
+map(m, ([prop, v]) => [prop, v * 2]);
+log(m);
+log(new Map(map(m, ([prop, v]) => [prop, v * 2])));
+
+
+log('------------------ Define Filter --------------');
+const filter = (iterator, predicate) => {
+    const result = [];
+    for (const item of iterator) {
+        if (predicate(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+};
+const over20000 = filter( function* () {
+    yield { price: 10000 };
+    yield { price: 20000 };
+    yield { price: 30000 };
+    yield { price: 40000 };
+}(), p => p.price > 20000);
+
+// log(over20000);
+
+log('------------------ Define Reduce --------------');
+const nums = [1, 2, 3, 4, 5];
+let total = 0;
+
+const reduce = (predicator, acc, iterator) => {
+    if (!iterator) {
+        iterator = acc[Symbol.iterator]();
+        acc = iterator.next().value;
+    }
+    for (const a of iterator) {
+        acc = predicator(acc, a);
+    }
+    return acc;
+};
+
+log(reduce((a, b) => a + b, nums));
