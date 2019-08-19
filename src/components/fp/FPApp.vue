8b5a2af35d4399c81da0953ba719b13dde98a725
kimtaehoon <kimtaehoon@smilegate.com>
2019-08-19 18:17:58 +0900

    [ADD] set render table temple

--- src/components/fp/FPApp.vue
+++ src/components/fp/FPApp.vue
@@ -12,18 +12,15 @@
                     <th scope="col">상품명</th>
                     <th scope="col">가격</th>
                     <th scope="col">수량</th>
-                    <th scope="col"></th>
+                    <th scope="col">합계</th>
                 </tr>
             </thead>
             <tbody>
                 <product></product>
                 <tr>
-                    <td>셔츠</td>
-                    <td>Price</td>
-                    <td>
-                        <label for="lbl_quanity"></label>
-                        <input type="number" value="" id="lbl_quanity">
-                    </td>
+                    <td>총합</td>
+                    <td></td>
+                    <td></td>
                     <td></td>
                 </tr>
             </tbody>
@@ -36,8 +33,15 @@ import { products } from '../common/constants';
 
 const Product = {
     name: 'Product',
+    functional: true,
     render(h) {
-        return h('div');
+        const trees = [];
+        for (const prod of products) {
+            const tds = [];
+            Object.keys(prod).forEach(prop => tds.push(h('td', prod[prop])));
+            trees.push(h('tr', [tds]));
+        }
+        return trees;
     }
 };
 export default {
@@ -56,5 +60,15 @@ export default {
 <style scoped>
 .tbl-cart{
     width: 840px;
+    border-collapse: collapse;
+}
+.tbl-cart thead th{
+    border-bottom: 2px solid black;
+}
+.tbl-cart td{
+    text-align: center;
+    padding: 4px 0;
+    border-bottom: 1px solid #eee;
+    border-right: 1px solid #eee;
 }
 </style>
