c682219c83d09fa2e3cbdc3a161a82ee5de60679
KimTaeHoon <wwwholakr@gmail.com>
2019-09-25 23:02:48 +0900

    [MOD] Serial reqeust complete fix error situation

--- src/components/auto/service/AutoCompleteService.ts
+++ src/components/auto/service/AutoCompleteService.ts
@@ -1,6 +1,6 @@
 import {injectable} from 'inversify';
-import {fromEvent, Observable, from, Subject, range, concat, Observer, interval} from 'rxjs';
-import {map, pluck, filter, take, mergeAll, mergeMap, tap, debounceTime, distinctUntilChanged} from 'rxjs/operators';
+import {fromEvent, Observable, from, Subject, range, concat, Observer, interval, of} from 'rxjs';
+import {map, pluck, filter, take, mergeAll, mergeMap, tap, debounceTime, distinctUntilChanged, catchError, switchMap, concatMap} from 'rxjs/operators';
 import {ajax} from 'rxjs/ajax';
 import {BASE_URL} from '@/components/common/constants';
 import {StarWarsPeople} from '@/components/auto/model/StarWarsResponse';
@@ -47,9 +47,37 @@ export class AutoCompleteService {
             error: e => error(`Observer B ${e}`),
             complete: () => log('Observer B Complete')
         }
-        intervalSubject.subscribe(observerA);
-        interval$.subscribe(intervalSubject);
-        setTimeout(() => intervalSubject.subscribe(observerB), 2000);
+        // intervalSubject.subscribe(observerA);
+        // interval$.subscribe(intervalSubject);
+        // setTimeout(() => intervalSubject.subscribe(observerB), 2000);
+
+        const querys: Array<string> = ['Luke', 'R2-D2', 'Darth', 'Leia','Owen', 'Beru', 'R5-D4', 'Biggs', 'Obi'];
+        // const luke$: Observable<any> =
+        ajax.getJSON(`https://swapi.co/api/?format=json`).pipe(
+            switchMap(res => of(res.people)),
+            switchMap(baseUrl => {
+                return from(querys).pipe(
+                    map((query => {
+                        if (['Owen', 'Biggs', 'Luke'].includes(query)) {
+                            return `http://lsajfdaoij.${query}feiofdl`;
+                        }
+                        else {
+                            return `${baseUrl}?search=${query}`;
+                        }
+                    })),
+                    concatMap(url => {
+                        return of(url).pipe(
+                            mergeMap(() => ajax.getJSON(url)),
+                            catchError(err => of(err.message)))
+                    }),
+                    pluck('results'),
+
+                )
+            })
+        ).subscribe(
+            v => console.log(v),
+            err => console.error(err)
+        )
 
     }
 }
