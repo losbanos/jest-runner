e566e8ed329323a8aab448f5aab63e0daf7fa778
KimTaeHoon <wwwholakr@gmail.com>
2020-01-23 00:29:15 +0900

    [config] alias 추가

---
+++ src/components/api/StarWarsService.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import {BaseUrl} from '@/components/enum/BaseUrl';
+
+export default class StarWarsService {
+    public pokemon(): void {
+        const poke: string = BaseUrl.Poke
+    }
+}
